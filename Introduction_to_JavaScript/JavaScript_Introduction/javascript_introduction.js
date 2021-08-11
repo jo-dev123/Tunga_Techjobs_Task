044cefc142109ef4e966068f044776301c5f0353
jodev123 <johnsondegbor@gmail.com>
2021-08-11 02:33:49 +0100

    adding value and primitive excercise

---
+++ Introduction_to_JavaScript/JavaScript_Introduction/javascript_introduction.js
@@ -0,0 +1,43 @@
+/* Create the following variables
+name, which is a string set to your current name
+dayOfBirth, which is a number set to the day of your birth month */
+
+let name = "Johnson",
+    dayOfBirth = 09;
+
+let name = "Johnson",
+    dayOfBirth = 09;
+
+console.log(dayOfBirth)
+
+
+// See what happens when you have multiple variables of the same name. Which one takes precedence?
+/* The last declared takes precedence*/
+
+// Write some JavaScript that prompt's the user for their favorite color. Once the user has submitted a favorite color, log that color to the console along with a friendly message.
+
+let askColor = prompt("Input Favorite Color");
+console.log("Its nice to know you like", askColor);
+
+// Create a string that contains both single quotes and double quotes.
+"Hi, this is a \"double quote\" and this is a 'single quote'";
+
+// What is the difference between null and undefined?
+/*
+null
+is an intention value assigned to denote a variable is empty
+
+undefined
+outputs when a value is not assigned to a value 
+*/
+
+// What is NaN in JavaScript? What is the typeof NaN?
+/*
+NaN 
+indicates that a type is not a number
+
+type of Nan is "number"
+*/
+
+// You can declare a variable by typing let thing;. What is the value of thing?
+undefined
